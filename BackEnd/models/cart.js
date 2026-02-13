ec5a4e0614b32e906a15823bf7f59d423771c4d2
NuthaN-444 <nuthanprasadkg@gmail.com>
2026-02-13 15:57:12 +0530

    small changes in Backend

--- BackEnd/models/cart.js
+++ BackEnd/models/cart.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const cartSchema = mongoose.Schema({
+const cartSchema = new mongoose.Schema({
     email:{
         type : String,
         required:true
--- BackEnd/models/product.js
+++ BackEnd/models/product.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
+const productSchema = new mongoose.Schema({
     productName:{
         type:String,
         required:true,
